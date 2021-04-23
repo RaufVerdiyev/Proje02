f4885ebe1b7616bc4a58cbce7680e14fa7e18a52
RaufVerdiyev <74197500+RaufVerdiyev@users.noreply.github.com>
2021-04-23 03:43:46 -0400

    reverse string ne version

--- src/PracticeG.java
+++ src/PracticeG.java
@@ -3,10 +3,24 @@ public class PracticeG {
 
         System.out.println("HelloGitHub");
         //How are you new KT
-        
+
         //Good Job Rauf
         // how are you Sir
-        
-        // good good good 
+
+        // good good good
+
+    String str ="apple";
+
+        reverseString(str, str.length() - 1);
+
+    }
+
+    public static void reverseString(String str, int number) {
+
+        if (number > -1) {
+
+            System.out.print(str.charAt(number));
+            reverseString(str, number - 1);
+        }
     }
 }
